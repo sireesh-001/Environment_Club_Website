9f0d21d53ffa4df1e2c31c56151f91d47214c1b2
Sireesh reddy <72874952+sireesh-001@users.noreply.github.com>
2022-11-24 00:22:06 +0530

    online DB connnection

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,11 +1,12 @@
 const mongoose = require("mongoose");
 
 
-mongoose.connect("mongodb://localhost:27017/EC",{
-    useNewUrlParser: true,
+mongoose.connect("mongodb+srv://environment:0WVgJMYJEyQJftwW@cluster0.odfxqev.mongodb.net/?retryWrites=true&w=majority",{
+    useNewUrlParser: true,   
     useUnifiedTopology: true,
     useCreateIndex:true
 
 }).then(()=>{
     console.log("Connection established succesfully");
 }).catch(()=>{console.log("error in establishing connection");})
+
